c2b3986d35bfa2d488fcdc662dd7bb9c9cf73a87
Hwi-sang Cho <hwisang@sas.upenn.edu>
2023-07-01 00:46:34 -0400

    first commit

---
+++ functions/.eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "google",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["tsconfig.json", "tsconfig.dev.json"],
+    sourceType: "module",
+  },
+  ignorePatterns: [
+    "/lib/**/*", // Ignore built files.
+    "/testing/**/**",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+  ],
+  rules: {
+    "quotes": ["off"],
+    "import/no-unresolved": 0,
+    "indent": ["error", 2],
+    "max-len": ["error", {"code": 120}],
+    "linebreak-style": 0,
+    "prefer-const": 0,
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: false,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
+      },
+    ],
+  },
+};
